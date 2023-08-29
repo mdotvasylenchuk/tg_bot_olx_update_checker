157976450064feede9f50a4962a815d740e0b150
VasylenchukMischa <mischa.blaster@gmail.com>
2023-08-29 18:05:23 +0300

    update Gemfile.lock

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ require 'time'
 require 'dotenv/load'
 
 Dotenv.load("#{__dir__}/.env")
-DB = Sequel.connect(adapter: :postgres, user: ENV["DB_USERNAME"], password: ENV["DB_PASSWORD"], host: 'localhost', port: 5432, database: ENV["DB_NAME"], max_connections: 10, logger: Logger.new("#{__dir__}/log/db.log"))
+DB = Sequel.connect(adapter: :postgres, user: ENV["DB_USERNAME"], password: ENV["DB_PASSWORD"], host: 'localhost', port: 5432, database: ENV["DB_NAME"], max_connections: 10)
 
 def request(url:)
   response = Faraday.get(url)
@@ -31,25 +31,20 @@ app_logger = Logger.new("#{__dir__}/log/app.log")
 urls = DB[:urls]
 loop do
   begin
-    parsed_data = Nokogiri::HTML5(request(url: ENV["OLX_URL"]))
+    parsed_data = Nokogiri::HTML5(request(url: ARGV[0]))
     chats = Telegram.bot.get_updates["result"]
-    if !!parsed_data
-      parsed_data.css('.css-rc5s2u').reverse.each do |e|
-        url_from_tag = e.attribute('href').value
-        if urls.where(url: url_from_tag).blank?
-          app_logger.info "There is new flat #{Time.now} #{url_from_tag}"
-          urls.insert(url: url_from_tag)
-          chats.map { |e| e.dig("message", "chat", "id") }&.uniq.each do |chat|
-            Telegram.bot.send_message chat_id: chat, text: "#{e.css('.css-veheph.er34gjf0').text.split(' - ').try(:[], 1)} - https://www.olx.ua#{url_from_tag}"
-          end
+    parsed_data.css('.css-rc5s2u').reverse.each do |e|
+      url_from_tag = e.attribute('href').value
+      if urls.where(url: url_from_tag).blank?
+        app_logger.info "There is new flat #{Time.now} #{url_from_tag}"
+        urls.insert(url: url_from_tag)
+        chats.map { |e| e.dig("message", "chat", "id") }&.uniq.each do |chat|
+          Telegram.bot.send_message chat_id: chat, text: "#{e.css('.css-veheph.er34gjf0').text.split(' - ').try(:[], 1)} - https://www.olx.ua#{url_from_tag}"
         end
       end
-    else
-      app_logger.info "No updates"
     end
     sleep 30
   rescue => e
     app_logger.fatal "Error #{e}"
-    Telegram.bot.send_message chat_id: ENV["TELEGRAM_MAIN_CHAT_ID"], text: "There is an error #{e}, try to restart bot by /start"
   end
 end
